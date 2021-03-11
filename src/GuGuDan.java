cf81cb0c553fec4a8ea782305125e187b7cd8890
김현석 <@DESKTOP-8G5I1U8>
2021-03-11 18:59:43 +0900

    원하는 단수 입력기능 추가

--- src/GuGuDan.java
+++ src/GuGuDan.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;
 
 public class GuGuDan {
 	public static void main(String[] args) {
-		for(int i = 2; i < 10; i++) {
-			for(int j = 1; j < 10; j++) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("몇 단을 출력하시겠습니까? :  ");
+		int i = sc.nextInt();
+		
+		for(int j = 1; j < 10; j++) {
 				System.out.printf("%d X %d = %d%n",i,j,i*j);
 			}
 		}
-	}
-
 }
+
+
